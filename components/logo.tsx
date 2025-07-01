e773e5f898689fc6dd547000b8e0189f9df5a33d
Leniors <mokualeeroy@gmail.com>
2025-07-01 20:36:18 +0300

    The Vault

---
+++ components/logo.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Link from "next/link";
+import { useUser } from "@/context/UserContext";
+
+const Logo = () => {
+  const { user } = useUser();
+
+  if (!user)
+    return (
+      <div className="fixed top-5 left-5 z-50 flex gap-4 items-center">
+        <Link href="/auth/register" className="flex items-center justify-center">
+          <img
+            src="/thevaultlogo.png"
+            alt="The Vault Logo"
+            className="w-12 h-12 md:w-40 md:h-40 object-cover rounded-full shadow-lg"
+          />
+        </Link>
+      </div>
+    );
+
+  return (
+    <div className='fixed top-5 left-5 z-50 flex gap-4 items-center'>
+        <Link href="/" className="flex items-center justify-center">
+        <img
+            src="/thevaultlogo.png"
+            alt="The Vault Logo"
+            className="w-12 h-12 md:w-40 md:h-40 object-cover rounded-full shadow-lg"
+        />
+        </Link>
+    </div>
+  );
+};
+
+export default Logo;
