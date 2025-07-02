6c686782b801028b9a257164743dce8341b31edb
Leniors <mokualeeroy@gmail.com>
2025-07-02 03:55:06 +0300

    The Vault

--- components/ui/card-hover-effect.tsx
+++ components/ui/card-hover-effect.tsx
@@ -83,17 +83,17 @@ export const HoverEffect = ({
                 </button>
               </div>
               {item.stock !== undefined && item.stock > 5 && (
-                <div className="absolute top-1 right-1 bg-green-600 text-white text-xs px-2 py-0.5 rounded">
+                <div className="absolute top-0 right-0 bg-green-600 text-white text-xs px-2 py-0.5 rounded">
                   In stock
                 </div>
               )}
               {item.stock !== undefined && item.stock < 5 && item.stock > 0 && (
-                <div className="absolute top-1 right-1 bg-red-600 text-white text-xs px-2 py-0.5 rounded">
+                <div className="absolute top-0 right-0 bg-red-600 text-white text-xs px-2 py-0.5 rounded">
                   Low stock
                 </div>
               )}
               {item.stock === 0 && (
-                <div className="absolute top-1 right-1 bg-red-600 text-white text-xs px-2 py-0.5 rounded">
+                <div className="absolute top-0 right-0 bg-red-600 text-white text-xs px-2 py-0.5 rounded">
                   Out of stock
                 </div>
               )}
